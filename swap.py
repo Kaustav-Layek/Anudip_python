49211fc6feb3367f6c9cfb849bea420a93d5273d
Kaustav Layek <181870708+Kaustav-Layek@users.noreply.github.com>
2024-12-20 13:31:30 +0530

    Lab3

---
+++ swap.py
@@ -0,0 +1,9 @@
+#taking inputs
+a=int(input("enter the first number"))
+print("First number is = ",a)
+
+b=int(input("enter the second number"))
+print("Second number is = ",b)
+ # swapping
+a, b=b, a
+print("After swapping first number is= ",a,"The second number is = ",b)
---
+++ ternary_if.py
@@ -0,0 +1,3 @@
+num=int(input("Ennter any number : "))
+is_even="The number is Even" if(num%2==0) else  "The number is Odd"
+print(is_even)
