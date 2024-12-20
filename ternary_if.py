c6b758bfc0ad9035ecd2d4d5bbda3b7df43615cc
Kaustav Layek <181870708+Kaustav-Layek@users.noreply.github.com>
2024-12-20 13:33:05 +0530

    Update ternary_if.py

--- ternary_if.py
+++ ternary_if.py
@@ -1,3 +1,4 @@
-num=int(input("Ennter any number : "))
-is_even="The number is Even" if(num%2==0) else  "The number is Odd"
+#checking a number is even or odd using ternary if
+num=int(input("Ennter any number : "))   #taking input
+is_even="The number is Even" if(num%2==0) else  "The number is Odd"  
 print(is_even)
